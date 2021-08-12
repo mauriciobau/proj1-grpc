b16efbbbd41258405b57e7f9093d0794aebcd6cc
Mauricio Antonio Bau <mauricio.bau@gmail.com>
2021-08-12 19:11:33 -0300

    Implementando serviço

---
+++ services/user_service.go
@@ -0,0 +1,28 @@
+package services
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/mauriciobau/proj1-grpc/pb"
+)
+
+type UserService struct {
+	pb.UnimplementedUserServiceServer
+}
+
+func NewUserService() *UserService {
+	return &UserService{}
+}
+
+func (*UserService) AddUser(ctx context.Context, req *pb.User) (*pb.User, error) {
+
+	// Insert - DataBase
+	fmt.Println(req.Name)
+
+	return &pb.User{
+		Id:    "123",
+		Name:  req.GetName(),
+		Email: req.GetEmail(),
+	}, nil
+}
