257c2f6d6a6c49365fda0420bf8d2ebb0daef6c8
Mauricio Antonio Bau <mauricio.bau@gmail.com>
2021-08-12 19:57:12 -0300

    Criadno client streaming

--- cmd/client/client.go
+++ cmd/client/client.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"time"
 
 	"github.com/mauriciobau/proj1-grpc/pb"
 	"google.golang.org/grpc"
@@ -20,7 +21,8 @@ func main() {
 	client := pb.NewUserServiceClient(connection)
 
 	//AddUser(client)
-	AddUserVerbose(client)
+	//AddUserVerbose(client)
+	AddUsers(client)
 }
 
 func AddUser(client pb.UserServiceClient) {
@@ -60,5 +62,61 @@ func AddUserVerbose(client pb.UserServiceClient) {
 		}
 		fmt.Println("Status:", stream.Status, " - ", stream.GetUser())
 	}
+}
 
+func AddUsers(client pb.UserServiceClient) {
+	reqs := []*pb.User{
+		&pb.User{
+			Id:    "1",
+			Name:  "Mauricio 1",
+			Email: "mauricio1@mail.com",
+		},
+		&pb.User{
+			Id:    "2",
+			Name:  "Mauricio 2",
+			Email: "mauricio2@mail.com",
+		},
+		&pb.User{
+			Id:    "3",
+			Name:  "Mauricio 3",
+			Email: "mauricio3@mail.com",
+		},
+		&pb.User{
+			Id:    "4",
+			Name:  "Mauricio 4",
+			Email: "mauricio4@mail.com",
+		},
+		&pb.User{
+			Id:    "5",
+			Name:  "Mauricio 5",
+			Email: "mauricio5@mail.com",
+		},
+		&pb.User{
+			Id:    "6",
+			Name:  "Mauricio 6",
+			Email: "mauricio6@mail.com",
+		},
+		&pb.User{
+			Id:    "7",
+			Name:  "Mauricio 7",
+			Email: "mauricio7@mail.com",
+		},
+	}
+
+	stream, err := client.AddUsers(context.Background())
+	if err != nil {
+		log.Fatalf("Error creating request: %v", err)
+	}
+
+	for _, req := range reqs {
+		stream.Send(req)
+		time.Sleep(time.Second * 3)
+	}
+
+	res, err := stream.CloseAndRecv()
+	if err != nil {
+		log.Fatalf("Error receiving response: %v", err)
+	}
+
+	fmt.Println(res)
 }
